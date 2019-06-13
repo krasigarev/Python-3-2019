29ca9a621a43f39c82b2453883dd6046e64767f1
Krasimir  Garev <kgarev@gmail.com>
2019-06-13 18:26:30 +0200

    Python Fundamentals – юни 2019

---
+++ lecture_1.py
@@ -0,0 +1,21 @@
+def print_top():
+    print("Cash Receipt\n"
+          "_____________________\n")
+
+def print_body():
+    print("Charged to __________\n"
+          "Receipt by __________\n")
+
+def print_footer():
+    print("_____________________\n"
+          "@ SoftUni\n")
+
+
+def print_receipt():
+    print_top()
+    print_body()
+    print_footer()
+
+
+
+print_receipt()
---
+++ test_01.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+
+def print_number():
+    for i in range(1, n+1, 5):
+        print(i, end=' ')
+
+
+print_number()
